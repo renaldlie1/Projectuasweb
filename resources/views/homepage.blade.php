826f7dbb09af3f0e240de50a602e2c030b5b0988
renaldlie <74336039+renaldlie@users.noreply.github.com>
2021-12-13 09:27:51 +0800

    Test

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -12,9 +12,9 @@
         <h1 class="text-center">Halaman Utama</h1>
         <p class="text-center"><strong>Website ini hanya menyediakan layanan Informasi & Permainan terkait Kuis Kebudayaan Indonesia</strong></p><br>
         <p class="text-center"><em>Untuk melihat referensi Pembelajaran bisa dilihat disini</em><a href="/vaksin"> Click</a></p>
-        <p class="text-center"><em>Untuk informasi atau bantuan lebih lanjut bisa klik disini</em><a href="/contact"> Click</a></p>
+        <p class="text-center"><em>Untuk informasi atau bantuan lebih lanjut bisa klik disini TESTES</em><a href="/contact"> Click</a></p>
     </div>
-    
 
-    
+
+
 @endsection
